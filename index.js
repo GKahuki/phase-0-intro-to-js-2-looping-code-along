8ff39457d5ce22f253521931d7c37a1d084b65e0
George Kahuki <utoriainvestmentsltd.com>
2025-04-21 00:02:35 +0300

    working with while and for loops

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solutions in this file
+// function writeCards(names, event) {
+//     for (let i = 0; i < names.length; i++) {
+//         const messages = [];
+//         console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+//         debugger;
+//     }
+//     return messages;
+// }
+// writeCards(["Guadalupe", "Ollie", "Aki"], "birthday");
+
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages;
+  }
+    const names = ["Guadalupe", "Ollie", "Aki"];
+  console.log(writeCards(names));
+
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+         }
+    return num;
+}
+countDown(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
